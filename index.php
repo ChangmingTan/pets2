396afdc7e1e2373527bef34ea0e6d7456dc5f702
Changming Tan <ctan9@mail.greenriver.edu>
2020-05-05 23:44:48 -0400

    added php validation for ordering

--- index.php
+++ index.php
@@ -1,15 +1,19 @@
 <?php
-//turn on error reporting
+
+//Turn on error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-//require the auto load file
+//Start a session
+session_start();
+
+//Require the autoload file
 require_once("vendor/autoload.php");
 
-//instantiate the F3 Base class
+//Instantiate the F3 Base class
 $f3 = Base::instance();
 
-//default route
+//Default route
 $f3->route('GET /', function () {
     //echo "<h1>My Pets</h1>";
 
@@ -19,7 +23,39 @@ $f3->route('GET /', function () {
 
 //Order route
 $f3->route('GET|POST /order', function ($f3) {
-    echo "Order Page";
+
+    //Check if the form has been posted
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        var_dump($_POST);
+
+        //Validate the data
+        $colors = array('purple', 'black', 'white');
+        if (empty($_POST['pet']) || !in_array($_POST['color'], $colors)) {
+            echo "<p>Please supply a pet type and select a color</p>";
+        } //Data is invalid
+        else {
+            //Data is valid
+            $_SESSION['pet'] = $_POST['pet'];
+            $_SESSION['color'] = $_POST['color'];
+
+            //Redirect to summary route
+            $f3->reroute('summary');
+            session_destroy();
+        }
+    }
+
+    $view = new Template();
+    echo $view->render('views/pet-order.html');
+
+});
+
+//Summary route
+$f3->route('GET /summary', function () {
+    //echo '<h1>Thank you for your order!</h1>';
+
+    $view = new Template();
+    echo $view->render('views/order-summary.html');
+
 });
 
 //run f3
